291bf54a6f4afb3b8b3f9b154c3051c847b6b69e
memray <memray0@gmail.com>
2017-12-01 17:05:43 -0500

    deploy to cluster

--- run_task.sh
+++ run_task.sh
@@ -7,12 +7,12 @@ for ((i=START;i<=END;i++)); do
     export i;
     for ((CONTEXT=1;CONTEXT<=3;i++)); do
         export CONTEXT=0
-        sbatch run_task.next.sbatch;
+        sbatch run_task.sbatch;
     done
 done
 
 export CONTEXT=0
 for ((i=START;i<=END;i++)); do
     export i;
-    sbatch run_task.next.similarity.sbatch;
+    sbatch run_task.similarity.sbatch;
 done
