00219f62a0d43db5de72063084026436cfcec1cb
memray <memray0@gmail.com>
2017-12-01 16:26:54 -0500

    deploy to cluster

--- run_task.next.sh
+++ run_task.next.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
-#SBATCH --cluster=smp
-#SBATCH --partition=smp
-#SBATCH --job-name=y_classify
-#SBATCH --output=next.run_task_log.out
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=4
 
-# Load modules
-#module restore
-
-# Run the job
+START=1
 END=8
-for ((i=1;i<=END;i++)); do
-    echo "python -m dialogue.classify.task_runner -selected_feature_set_id $i -selected_context_id 0"
-    srun python -m dialogue.classify.task_runner -selected_feature_set_id $i -selected_context_id 0
+
+for ((i=START;i<=END;i++)); do
+    sbatch run_task.next.sbatch
 done
