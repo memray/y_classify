0c6af04dd5b0e8352ab77796d5943193fe9babd5
memray <memray0@gmail.com>
2017-12-02 18:58:50 -0500

    deploy to cluster

--- run_task.sh
+++ run_task.sh
@@ -9,7 +9,7 @@ do
     for ((i=START;i<=END;i++)); do
         export i;
         echo "Context=$CONTEXT, Feature=$i";
-        sbatch --export=CONTEXT=$CONTEXT,i=$i,PYTHONPATH=/ihome/pbrusilosky/rum20/.conda/envs/py35/bin/python --job-name=context-$CONTEXT.feature-$i.run --output=slurm_log/context-$CONTEXT.feature-$i.run_task_log.out run_task.sbatch;
+        sbatch --export=CONTEXT=$CONTEXT,i=$i --job-name=context-$CONTEXT.feature-$i.run --output=slurm_log/context-$CONTEXT.feature-$i.run_task_log.out run_task.sbatch;
     done
 done
 
