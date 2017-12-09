38a1e13924d24689ee45511d77674635e68e5f52
memray <memray0@gmail.com>
2017-12-09 03:02:37 -0500

    rerun model selection, lr+svm,c=-2

--- model_selection.feature=7_d2v.sh
+++ model_selection.feature=7_d2v.sh
@@ -2,6 +2,6 @@
 
 export CONTEXT=0;
 export i=7;
-export CLASSIFIER="lr_svm_c=-3";
+export CLASSIFIER="lr_svm_c=-2";
 echo "Context=$CONTEXT, Feature=$i";
 sbatch --export=CONTEXT=$CONTEXT,i=$i --job-name=context-$CONTEXT.feature-$i.run --output=slurm_log/classifier-$CLASSIFIER.context-$CONTEXT.feature-$i.out run_task.sbatch;
