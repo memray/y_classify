4f8bee5058c74ce4a23ba133568f71331842db56
memray <memray0@gmail.com>
2017-12-09 03:14:52 -0500

    rerun model selection, lr+svm, c=4

--- model_selection.feature=2_lexical.sh
+++ model_selection.feature=2_lexical.sh
@@ -2,6 +2,6 @@
 
 export CONTEXT=0;
 export i=2;
-export CLASSIFIER="lr_svm_c=3";
+export CLASSIFIER="lr_svm_c=4";
 echo "Context=$CONTEXT, Feature=$i";
 sbatch --export=CONTEXT=$CONTEXT,i=$i --job-name=context-$CONTEXT.feature-$i.run --output=slurm_log/classifier-$CLASSIFIER.context-$CONTEXT.feature-$i.out run_task.sbatch;
