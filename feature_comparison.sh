ef104450e05a166457c2609f7f79335c6b102e25
memray <memray0@gmail.com>
2018-01-02 16:36:12 -0500

    bug

--- feature_comparison.sh
+++ feature_comparison.sh
@@ -6,15 +6,15 @@ export EXP_MODE='cross_validation'
 START=0
 END=14
 
-for ((CONTEXT=0;CONTEXT<=3;CONTEXT++));
-do
-    export CONTEXT
-    for ((FEATURE_ID=START;FEATURE_ID<=END;FEATURE_ID++)); do
-        export FEATURE_ID;
-        echo "$EXP_MODE, Context=$CONTEXT, Feature=$FEATURE_ID";
-        sbatch --export=CONTEXT=$CONTEXT,FEATURE_ID=$FEATURE_ID,EXP_MODE=$EXP_MODE --job-name=$EXP_MODE.context-$CONTEXT.feature-$FEATURE_ID.run --output=slurm_log/$EXP_MODE.context-$CONTEXT.feature-$FEATURE_ID.run_task_log.out run_task.sbatch;
-    done
-done
+#for ((CONTEXT=0;CONTEXT<=3;CONTEXT++));
+#do
+#    export CONTEXT
+#    for ((FEATURE_ID=START;FEATURE_ID<=END;FEATURE_ID++)); do
+#        export FEATURE_ID;
+#        echo "$EXP_MODE, Context=$CONTEXT, Feature=$FEATURE_ID";
+#        sbatch --export=CONTEXT=$CONTEXT,FEATURE_ID=$FEATURE_ID,EXP_MODE=$EXP_MODE --job-name=$EXP_MODE.context-$CONTEXT.feature-$FEATURE_ID.run --output=slurm_log/$EXP_MODE.context-$CONTEXT.feature-$FEATURE_ID.run_task_log.out run_task.sbatch;
+#    done
+#done
 
 export CONTEXT=0
 for ((FEATURE_ID=START;FEATURE_ID<=END;FEATURE_ID++));
