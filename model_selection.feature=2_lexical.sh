02812dc315ac61fb75af9c9b8fe3f96cb3878194
memray <memray0@gmail.com>
2018-02-20 10:19:04 -0500

    run svm l1 c=1.0, context=all, similarity=true

--- model_selection.feature=2_lexical.sh
+++ model_selection.feature=2_lexical.sh
@@ -3,7 +3,7 @@
 export EXP_MODE='cross_validation'
 export CONTEXT=3; # context=all
 export FEATURE_ID=2;
-export CLASSIFIER="svm_l1_c=0.5";
+export CLASSIFIER="svm_l1_c=1.0";
 echo "Context=$CONTEXT, Feature=$FEATURE_ID, Classifier=$CLASSIFIER";
 
 sbatch --export=CONTEXT=$CONTEXT,FEATURE_ID=$FEATURE_ID,EXP_MODE=$EXP_MODE --job-name=classifier-$CLASSIFIER.$EXP_MODE.context-$CONTEXT.feature-$FEATURE_ID.similarity.run --output=slurm_log/classifier-$CLASSIFIER.$EXP_MODE.context-$CONTEXT.feature-$FEATURE_ID.similarity.run_task_log.out run_task.similarity.sbatch;
