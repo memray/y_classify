e2a79590dd7fc88c922db3a3cfe6dc0902be299f
memray <memray0@gmail.com>
2018-02-19 23:23:14 -0500

    run rf #tree=1024, context=all, similarity=true

--- model_selection.feature=2_lexical.sh
+++ model_selection.feature=2_lexical.sh
@@ -3,7 +3,7 @@
 export EXP_MODE='cross_validation'
 export CONTEXT=3; # context=all
 export FEATURE_ID=2;
-export CLASSIFIER="rf_tree512";
-echo "Context=$CONTEXT, Feature=$FEATURE_ID";
+export CLASSIFIER="rf_tree1024";
+echo "Context=$CONTEXT, Feature=$FEATURE_ID, Classifier=$CLASSIFIER";
 
 sbatch --export=CONTEXT=$CONTEXT,FEATURE_ID=$FEATURE_ID,EXP_MODE=$EXP_MODE --job-name=classifier-$CLASSIFIER.$EXP_MODE.context-$CONTEXT.feature-$FEATURE_ID.similarity.run --output=slurm_log/classifier-$CLASSIFIER.$EXP_MODE.context-$CONTEXT.feature-$FEATURE_ID.similarity.run_task_log.out run_task.similarity.sbatch;
