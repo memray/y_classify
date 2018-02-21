9d23443bbb87837643e259ab04950f4c4b9c4619
memray <memray0@gmail.com>
2018-02-21 14:42:01 -0500

    run 9 16 17 18, continuous feature selection, discrete=256, similarity=false

--- continuous_feature_selection.sh
+++ continuous_feature_selection.sh
@@ -6,7 +6,7 @@ export EXP_MODE='continuous_feature_selection'
 # 20180123, do feature selection on 15-18: [1,2,3,4] + one of [5,6,7,8]
 
 export CONTEXT=0
-declare -a FEATURE_IDs=(0 15)
+declare -a FEATURE_IDs=(16 17 18)
 declare -a power=(3 4 5 6 7 8) # (2 3 4 5 6 7 8 9 10 -1)
 
 export DISCRETE_NUMBER_TO_KEEP=8
--- discrete_feature_selection.sh
+++ discrete_feature_selection.sh
@@ -5,7 +5,7 @@ export EXP_MODE='discrete_feature_selection'
 # do feature selection on 9 and 11
 
 export CONTEXT=0
-declare -a FEATURE_IDs=(1 2 3 4) # (0 9 10 11 12 13 14)
+declare -a FEATURE_IDs=(9) # (0 9 10 11 12 13 14)
 declare -a power=(4 5 6 7 8 9 10 11 12 -1)
 
 for FEATURE_ID in "${FEATURE_IDs[@]}"
@@ -14,7 +14,8 @@ do
     for NUMBER_TO_KEEP in "${power[@]}"
     do
         export NUMBER_TO_KEEP;
-        echo "$EXP_MODE, Feature_Number_TO_KEEP=$NUMBER_TO_KEEP, Context=$CONTEXT, Feature_ID=$FEATURE_ID, with similarity";
-        sbatch --export=CONTEXT=$CONTEXT,FEATURE_ID=$FEATURE_ID,NUMBER_TO_KEEP=$NUMBER_TO_KEEP,EXP_MODE=$EXP_MODE --job-name=$EXP_MODE.feature_num-$NUMBER_TO_KEEP.context-$CONTEXT.feature-$FEATURE_ID.similarity.run --output=slurm_log/$EXP_MODE.feature_num-$NUMBER_TO_KEEP.context-$CONTEXT.feature-$FEATURE_ID.similarity.out run_task.similarity.sbatch;
+#        echo "$EXP_MODE, Feature_Number_TO_KEEP=$NUMBER_TO_KEEP, Context=$CONTEXT, Feature_ID=$FEATURE_ID, with similarity";
+#        sbatch --export=CONTEXT=$CONTEXT,FEATURE_ID=$FEATURE_ID,NUMBER_TO_KEEP=$NUMBER_TO_KEEP,EXP_MODE=$EXP_MODE --job-name=$EXP_MODE.feature_num-$NUMBER_TO_KEEP.context-$CONTEXT.feature-$FEATURE_ID.similarity.run --output=slurm_log/$EXP_MODE.feature_num-$NUMBER_TO_KEEP.context-$CONTEXT.feature-$FEATURE_ID.similarity.out run_task.similarity.sbatch;
+        sbatch --export=CONTEXT=$CONTEXT,FEATURE_ID=$FEATURE_ID,NUMBER_TO_KEEP=$NUMBER_TO_KEEP,EXP_MODE=$EXP_MODE --job-name=$EXP_MODE.feature_num-$NUMBER_TO_KEEP.context-$CONTEXT.feature-$FEATURE_ID.similarity.run --output=slurm_log/$EXP_MODE.feature_num-$NUMBER_TO_KEEP.context-$CONTEXT.feature-$FEATURE_ID.similarity.out run_task.sbatch;
     done
 done
